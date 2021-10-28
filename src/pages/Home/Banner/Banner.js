f0b29848f0a47a428fe4f9c339ea30c0f34b8c3f
Rony163 <minhajulislamrony43@gmail.com>
2021-10-29 02:52:27 +0600

    banner added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import './Banner.css';
 
 const Banner = () => {
     return (
         <div>
-            <h1>This is banner</h1>
+            <Carousel variant="dark" className="mt-5" >
+                <Carousel.Item className="mt-2">
+                    <img
+                        className="d-block banner"
+                        src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/summertime-background-img-5.jpg"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption className="text-white bannerText">
+                        <h1>Explore travel and make some fun!!!</h1>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="mt-2">
+                    <img
+                        className="d-block banner"
+                        src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/summertime-background-img-4.jpg"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption className="text-white bannerText">
+                        <h1>Explore travel and make some fun!!!</h1>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="mt-2">
+                    <img
+                        className="d-block banner"
+                        src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/summertime-background-img-6.jpg"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption className="text-white bannerText">
+                        <h1>Explore travel and make some fun!!!</h1>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
