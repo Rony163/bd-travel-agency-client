c0cd287b3ec432799445e84e013ac5f4fe16d3ff
Rony163 <minhajulislamrony43@gmail.com>
2021-10-29 01:28:45 +0600

    added some components complete not found page and cdn link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+import Footer from './pages/Shared/Footer/Footer';
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
