98a7467a397d23a79fac48be59b04710c0ee5c86
Rony163 <minhajulislamrony43@gmail.com>
2021-10-30 20:55:05 +0600

    update booking system

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useState, useEffect } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
 import useAuth from '../../hooks/useAuth';
 import './Details.css';
@@ -10,6 +9,9 @@ const Details = () => {
     const { id } = useParams();
     const { user } = useAuth();
     const [service, setService] = useState({});
+    const refUserName = useRef('')
+    const refEmail = useRef('')
+    const [singleService, setSingleService] = useState({});
 
     // load data from database
     useEffect(() => {
@@ -18,32 +20,48 @@ const Details = () => {
             .then(data => setService(data))
     }, [id])
 
-    const preloadValues = {
-        name: user?.displayName,
-        email: user?.email,
+    const handlePersonChange = e => {
+        const updatePerson = e.target.value;
+        const updateEvent = { ...singleService };
+        updateEvent.person = updatePerson;
+        setSingleService(updateEvent)
+    }
+    const handlePhoneNumberChange = e => {
+        const updatePhone = e.target.value;
+        const updateEvent = { ...singleService };
+        updateEvent.mobile = updatePhone;
+        setSingleService(updateEvent)
+    }
+    const handleDateOfTravel = e => {
+        const updateDuration = e.target.value;
+        const updateEvent = { ...singleService };
+        updateEvent.date = updateDuration;
+        setSingleService(updateEvent)
     }
 
-    const { register, handleSubmit, reset } = useForm({
-        defaultValues: preloadValues
-    });
+    const handleSubmit = e => {
+        singleService.name = refUserName.current.value;
+        singleService.email = refEmail.current.value;
+        singleService.ServiceName = service?.name;
+        singleService.status = 'pending';
+        console.log(singleService);
 
-
-    const onSubmit = data => {
-        data.status = "pending";
         fetch('http://localhost:5000/events', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(singleService)
         })
             .then(res => res.json())
             .then(result => {
                 if (result.insertedId) {
                     alert("Registration successful");
-                    reset();
+                    setService({});
+                    e.target.reset();
                 }
             })
+        e.preventDefault();
     };
 
     return (
@@ -59,7 +77,18 @@ const Details = () => {
                 <Col xs={12} md={6}>
                     <div>
                         <h1>Booking Here</h1>
-                        <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <form onSubmit={handleSubmit}>
+                            <input className="input-field" ref={refUserName} type="text" placeholder="Name" value={user.displayName || ''} />
+                            <input className="input-field" ref={refEmail} type="email" placeholder='Email' value={user.email || ''} />
+                            <input className="input-field" onChange={handlePersonChange} type="number" placeholder='Total Person' />
+                            <input className="input-field" onChange={handlePhoneNumberChange} type="number" placeholder='Phone Number' />
+                            <input className="input-field" onChange={handleDateOfTravel} type="number" placeholder='Date of Travel' />
+                            <input className="input-field" type="submit" value="Register" />
+                        </form>
+
+
+                        {/* <form onSubmit={handleSubmit(onSubmit)}>
                             <input className="input-field"{...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
 
                             <input className="input-field"{...register("email", { required: true, maxLength: 30 })} placeholder="Email" />
@@ -73,7 +102,7 @@ const Details = () => {
                             <input className="input-field"{...register("ServiceName")} placeholder="Service name" value={service.name || ''} />
 
                             <input className='input-field btn-submit' type="submit" value="Submit" />
-                        </form>
+                        </form> */}
 
                     </div>
                 </Col>
